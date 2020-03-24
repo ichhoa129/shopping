30e2e018f9235a70a1addbb9c1d90b72d961755f
IchHoa <ichhoa129@gmail.com>
2020-03-24 17:58:03 +0700

    random id and view users

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const low = require('lowdb');
+const shortid = require('shortid');
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json');
 const db =  low(adapter);
-
+//set database
 db.defaults({ users:[]})
   .write();
 
@@ -16,22 +17,31 @@ app.set('views', './views');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 //render index page
 app.get('/',(req,res) => res.render('index'));
 //get and show users
 app.get('/users', (req,res) =>res.render('users/index',{
     users: db.get('users').value()
 }));
-//search user matched chars
-app.get('/users/search',(req,res)=>{
-    var q = req.query.q;
-    var matchedUsers = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1 );
-    res.render('users/index',{users: matchedUsers});
-}); 
-//create users
 app.get('/users/create',(req,res)=>res.render('users/create'));
+//search user matched chars
+// app.get('/users/search',(req,res)=>{
+//     var q = req.query.q;
+//     var matchedUsers = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1 );
+//     res.render('users/index',{users: matchedUsers});
+// }); 
+//get id and render from db
+app.get('/users/:id',(req,res)=>{
+    var id = req.params.id;
+    var user = db.get('users').find({ id: id}).value();
+    res.render('users/view',{ user: user})
+});
+
+//create users
 app.post('/users/create',(req,res)=>{
-    db.get('users').push(req.body).write();
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write(); 
      res.redirect('/users');
 });
 
