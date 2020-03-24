e6b74ab0cf0d8151e141626a0f7dac5374fd28ce
IchHoa <ichhoa129@gmail.com>
2020-03-25 02:11:44 +0700

    add controller file follow MVC

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,33 +1,18 @@
-var express = require('express')
-var router = express.Router()
-const shortid = require('shortid')
-const db = require('../db')
+const    express = require('express')
+const router = express.Router()
+const controller = require('../controllers/user.controller')
+
+
 
 //get and show users
-router.get('/', (req,res) =>res.render('users/index',{
-    users: db.get('users').value()
-}));
-router.get('/create',(req,res)=>res.render('users/create'));
+router.get('/',controller.index);
+router.get('/create',controller.create);
 //search user matched chars
-router.get('/search',(req,res)=>{
-    var q = req.query.q; //what
-    users = db.get('users').value();
-    var matchedUsers = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1 );
-    res.render('users/index',{users: matchedUsers});
-}); 
+router.get('/search',controller.search); 
 //get id and render from db for view
-router.get('/view/:id',(req,res)=>{
-    var id = req.params.id;
-    console.log(id);
-    var user = db.get('users').find({ id: id}).value();
-    res.render('users/view',{ user: user})
-});
+router.get('/view/:id',controller.viewId);
 
 //create users
-router.post('/create',(req,res)=>{
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write(); 
-     res.redirect('/users');
-});
+router.post('/create',controller.postCreate);
 
 module.exports = router;
